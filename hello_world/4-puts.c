8a1ab31ea99979d437fc5cbadafe2a4ff448adae
Bushra2252 <10949@holbertonschool.com>
2025-04-24 06:55:07 +0000

    add4

---
+++ hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - This function starts the program and prints a message
+*
+* Return: 0 if success
+*/
+int main(void)
+{
+   puts("\"Programming is like building a multilingual puzzle");
+   return (0);
+}
+
