c5cea9c3db14905c97485a53819e94b0a37afa2f
Bushra2252 <10949@holbertonschool.com>
2025-05-01 06:33:16 +0000

    add2

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -2,15 +2,17 @@
 
 /**
 * print_alphabet_x10 - prints the alphabet 10 times in lowercase
+*
+* Return: void
 */
 void print_alphabet_x10(void)
 {
 int num;
 char ch;
 
-for(num=1; num<=10; num++)
+for (num = 1; num <= 10; num++)
 {
-for(ch='a'; ch<='z'; ch++)
+for (ch = 'a'; ch <= 'z'; ch++)
 {
 _putchar(ch);
 }
