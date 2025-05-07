bdafc0fbb970dcabfd73acba2c1ef4cd950cd48f
Your Name <you@example.com>
2025-05-07 07:06:52 +0000

    add3

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -6,12 +6,12 @@
 *
 *Return: void
 */
-void_puts(char *str)
+void _puts(char *str)
 {
-while (*str '\0')
+while (*str '\0');
 {
-putchar(*str);
+putchar (*str);
 str++;
 }
-putchar('\n')
+putchar ('\n');
 }
