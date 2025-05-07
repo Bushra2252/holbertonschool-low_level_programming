ea68d02e443c35b06958fab471278565258a5797
Your Name <you@example.com>
2025-05-07 07:04:00 +0000

    add3

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*_puts - print
+*@str: po
+*
+*Return: void
+*/
+void_puts(char *str)
+{
+while (*str '\0')
+{
+putchar(*str);
+str++;
+}
+putchar('\n')
+}
