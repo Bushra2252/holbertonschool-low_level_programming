15a059e1863c71653acba76fb5a7c7c5d05e2bcc
you@example.com <you@example.com>
2025-05-08 12:26:20 +0000

    add10

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,43 +1,46 @@
 #include "main.h"
 
 /**
-* _atoi - converts a string to an integer
-* @s: the string to convert
-*
-* Return: the integer value
-*/
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ *
+ * Return: the int converted from the string
+ */
 int _atoi(char *s)
 {
-int i = 0;
-int sign = 1;
-int result = 0;
-int digit;
+	int i, d, n, len, f, digit;
 
-while (s[i] != '\0')
-{
-if (s[i] == '-')
-sign *= -1;
-else if (s[i] >= '0' && s[i] <= '9')
-break;
-i++;
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && f == 0)
+	{
+		if (s[i] == '-')
+			++d;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++;
+	}
+
+	if (f == 0)
+		return (0);
+
+	return (n);
 }
-
-if (s[i] == '\0')
-return (0);
-
-while (s[i] >= '0' && s[i] <= '9')
-{
-digit = s[i] - '0';
-
-if (sign == 1 && (result > (2147483647 - digit) / 10))
-return (2147483647);
-if (sign == -1 && (result > (2147483648U - digit) / 10))
-return (-2147483648);
-
-result = result * 10 + digit;
-i++;
-}
-
-return (sign * result);
-}
-
