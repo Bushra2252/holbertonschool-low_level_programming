b6f3f6113aa2e8cd5d6de4b8e46ab8ad4a1a8720
you@example.com <you@example.com>
2025-05-08 12:22:09 +0000

    add10

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -10,36 +10,34 @@ int _atoi(char *s)
 {
 int i = 0;
 int sign = 1;
-unsigned int result = 0;
-int started = 0;
+int result = 0;
+int digit;
 
 while (s[i] != '\0')
 {
-if (s[i] == '-' && !started)
-{
+if (s[i] == '-')
 sign *= -1;
-}
 else if (s[i] >= '0' && s[i] <= '9')
-{
-result = result * 10 + (s[i] - '0');
-started = 1;
-}
-else if (started)
-{
 break;
-}
 i++;
 }
 
-if (!started)
-{
+if (s[i] == '\0')
 return (0);
+
+while (s[i] >= '0' && s[i] <= '9')
+{
+digit = s[i] - '0';
+
+if (sign == 1 && (result > (2147483647 - digit) / 10))
+return (2147483647);
+if (sign == -1 && (result > (2147483648U - digit) / 10))
+return (-2147483648);
+
+result = result * 10 + digit;
+i++;
 }
 
-if (sign == -1)
-{
-return (-((int)result));
-}
-return ((int)result);
+return (sign * result);
 }
 
