9afce2921810fbe959f2a8dcf699d2c0453d7f40
Your Name <you@example.com>
2025-05-17 04:47:48 +0000

    add

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+*main - prints
+*@argc: number
+*@argv: array
+*
+*Return:always 
+*/
+int main(int argc, char *argv[])
+{
+(void)args;
+printf("%s\n", argv[0]);
+return (0);
+}
