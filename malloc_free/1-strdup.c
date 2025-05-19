884e55deacdeca8b1dd0df4fb218c05c65820cf7
Bushra2252 <10949@holbertonstudents.com>
2025-05-19 13:51:57 +0000

    add1

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+*_strdup - duplicates
+*@str: the
+*
+*Return: pointer
+*/
+char *_strdup(char *str)
+{
+char *copy;
+int i = 0, len = 0;
+
+if (str == NULL)
+return (NULL);
+
+while (str[len] != '\0')
+len++;
+
+copy = malloc(sizeof(char) * (len + 1));
+
+while (i < len)
+{
+copy[i] = str[i];
+i++
+}
+copy[i] = '\0';
+
+return (copy);
+}
