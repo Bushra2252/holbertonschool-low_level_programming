6fa51a9bb0c358ff00bba9f0dfa51f26568890c6
Bushra2252 <10949@holbertonstudents.com>
2025-05-20 15:12:14 +0000

    add2

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+*_calloc - allocates
+*@nmemb: Number
+*@size: size
+*
+*Return: pointer
+*/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+char *array;
+unsigned int i;
+
+if (nmemb == 0 || size == 0)
+return (NULL);
+
+array = malloc(nmemb * size);
+if (array == NULL)
+return (NULL);
+
+for (i = 0; i < (nmemb * size); i++)
+array[i] = 0;
+
+return (array);
+}
