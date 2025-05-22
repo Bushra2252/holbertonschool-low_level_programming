8beeabecf9ea02485e8687c47b1840e1af17874d
Bushra2252 <10949@holbertonstudents.com>
2025-05-22 06:53:56 +0000

    add4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -14,6 +14,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 dog_t *dog2;
 char *name2, *owner2;
+int i, len1, len2;
 
 if (name == NULL || owner == NULL)
 return (NULL);
@@ -28,7 +29,9 @@ if (name2 == NULL)
 free(dog2);
 return (NULL);
 }
-strcpy(name2, name);
+for (i = 0; i < len1; i++)
+name2[i] = name[i];
+name2[i] = '\0';
 
 owner2 = malloc(strlen(owner) + 1);
 if (owner2 == NULL)
@@ -37,7 +40,9 @@ free(name2);
 free(dog2);
 return (NULL);
 }
-strcpy(owner2, owner);
+for (i = 0; i < len2; i++)
+owner2[i] = owner[i];
+owner2[i] = '\0';
 
 dog2->name = name2;
 dog2->age = age;
